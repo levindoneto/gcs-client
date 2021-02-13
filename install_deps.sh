18b35786c16892503257f209cded43266f7bcff4
root <levindogtn@gmail.com>
2021-02-13 15:23:20 -0300

    Update init sh

--- install_deps.sh
+++ install_deps.sh
@@ -1,3 +1,5 @@
 pip install --upgrade google-api-python-client
 pip install --upgrade google-cloud-bigquery
 pip install --upgrade google-cloud-storage
+
+export GOOGLE_APPLICATION_CREDENTIALS="/home/levindo.neto/Desktop/gcloud/gcs-client/MyFirstProject-39fc0f84b82a.json
